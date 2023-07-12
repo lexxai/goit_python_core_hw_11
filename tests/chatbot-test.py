d92d62efc75c8311d7f01edbdf2307343d4c3119
lexxai <lexxai@users.noreply.github.com>
2023-07-12 07:16:12 +0300

    tune version and tests

--- tests/chatbot-test.py
+++ tests/chatbot-test.py
@@ -1,23 +1,23 @@
 import sys
 sys.path.append('../src')
 
-from chatbot import main
+from chatbot import main as ChatBot
 
 if __name__ == "__main__":
-    main.COMMANDS["add"]("Jon-00", "+38044333223", "3344")
-    main.COMMANDS["add birthday"]("Jon-00", "1988-02-17")
-    main.COMMANDS["add"]("Jon-01", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-02", "+38044333223", "3344")  
-    main.COMMANDS["add"]("Jon-03", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-04", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-05", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-06", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-07", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-08", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-09", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-10", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-11", "+38044333223", "3344")
-    main.COMMANDS["add"]("Jon-12", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-00", "+38044333223", "3344")
+    ChatBot.COMMANDS["add birthday"]("Jon-00", "1988-02-17")
+    ChatBot.COMMANDS["add"]("Jon-01", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-02", "+38044333223", "3344")  
+    ChatBot.COMMANDS["add"]("Jon-03", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-04", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-05", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-06", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-07", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-08", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-09", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-10", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-11", "+38044333223", "3344")
+    ChatBot.COMMANDS["add"]("Jon-12", "+38044333223", "3344")
 
-    main.main()
+    ChatBot.main()
 
